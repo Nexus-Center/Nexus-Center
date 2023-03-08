3d19095331cdd54c612645350508d7bc5cc17d7d
vmirella04 <vmirella54@gmail.com>
2023-03-08 18:58:00 -0300

    adicionando banco de dados e JS do cadastro

--- site/public/js/cadastro.js
+++ site/public/js/cadastro.js
@@ -1,3 +1,69 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".menu_lateral").classList.toggle("show-menu")
 );
+
+function cadastrar() {
+    aguardar();
+
+    //Recupere o valor da nova input pelo nome do id
+    // Agora vá para o método fetch logo abaixo
+    var varNome = nome_input.value;
+    var varEmail = email_input.value;
+    var varSenha = senha_input.value;
+    var varConfirmacaoSenha = confirmacao_senha_input.value;
+    var varCodigoEmpresa = codigo_empresa_input.value;
+
+    if (varNome == "" || varEmail == "" || varSenha == "" || varConfirmacaoSenha == "" || varCodigoEmpresa == "") {
+        cardErro.style.display = "block"
+        mensagem_erro.innerHTML = "(Mensagem de erro para todos os campos em branco)";
+
+        finalizarAguardar();
+        return false;
+    }
+    else {
+        setInterval(sumirMensagem, 5000)
+    }
+
+    // Enviando o valor da nova input
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            // crie um atributo que recebe o valor recuperado aqui
+            // Agora vá para o arquivo routes/usuario.js
+            nomeServer: varNome,
+            emailServer: varEmail,
+            senhaServer: varSenha,
+            codigoServer: varCodigoEmpresa
+        })
+    }).then(function (resposta) {
+
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+            cardErro.style.display = "block";
+
+            mensagem_erro.innerHTML = "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+
+            setTimeout(() => {
+                window.location = "login.html";
+            }, "2000")
+
+            limparFormulario();
+            finalizarAguardar();
+        } else {
+            throw ("Houve um erro ao tentar realizar o cadastro!");
+        }
+    }).catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+        finalizarAguardar();
+    });
+
+    return false;
+}
+
+function sumirMensagem() {
+    cardErro.style.display = "none"
+}
--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,99 +1,41 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+-- Criar Database
+create database NexusCenter;
 
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
+-- Selecionar Database
+use NexusCenter;
 
-CREATE DATABASE aquatech;
+-- Criar tabelas
 
-USE aquatech;
+CREATE TABLE CadastroFuncionario (
+    idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    CONSTRAINT chkEmail CHECK (email LIKE '%@%'),
+    senha VARCHAR(45),
+    codigoDeEmpresa INT,
+    codigoDeEquipe INT,
+    setor VARCHAR (5),
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE Maquina (
+    idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+    setor VARCHAR(45),
+    fkFuncionario INT,
+    FOREIGN KEY (fkFuncionario)
+        REFERENCES CadastroFuncionario (idFuncionario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
+CREATE TABLE Metricas (
+    idmetrica INT AUTO_INCREMENT,
+    dataHora DATETIME,
+    cpu DECIMAL(4 , 2 ),
+    ram DECIMAL(4 , 2),
+    memoria DECIMAL(4 , 2 ),
+    periferico DECIMAL(4 , 2),
+    fkMaquina  INT,
+    PRIMARY KEY (idmetrica , fkMaquina),
+    FOREIGN KEY (fkMaquina)
+        REFERENCES Maquina (idMaquina)
+)
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
-
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
