2693a2a780104740e76b70fd00e0675678dc4edc
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-04-06 12:56:24 -0300

    Script banco de dados atualizado

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -2,7 +2,7 @@ CREATE DATABASE nexusCenter;
 USE nexusCenter;
 
 CREATE TABLE empresa(
-idEmpresa varchar(6) primary key,
+idEmpresa char(6) primary key,
 nome varchar(120),
 cnpj char(14) not null,
 email varchar(45),
@@ -17,9 +17,8 @@ idUser int primary key auto_increment,
 nome varchar(45),
 email varchar(45),
 senha varchar(45),
-codigo varchar(6),
-FOREIGN KEY (codigo) REFERENCES empresa(idEmpresa)
+fkEmpresa char(6),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 SELECT * FROM usuario;
-
