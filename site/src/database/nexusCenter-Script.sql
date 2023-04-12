dbeae25c514dfb0da201731239b4a16acfbe619e
BRyuTakahashi <bruno.takahashi@sptech.school>
2023-04-12 20:34:53 -0300

    adicionando banco de dados da nova modelagem (12/04)

--- site/src/database/nexusCenter-Script.sql
+++ site/src/database/nexusCenter-Script.sql
@@ -36,7 +36,9 @@ cordenadaY INT,
 fkMaquina INT,
 FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
 fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
+qntClicks VARCHAR(45),
+dataHora DATETIME
 );
 
 CREATE TABLE RegistroAtividade(
@@ -55,70 +57,45 @@ permissoes VARCHAR(45)
 );
 
 CREATE TABLE Componente(
-idComponente INT PRIMARY KEY,
-tipoComponente VARCHAR(45),
-nome VARCHAR(45),
-modelo VARCHAR(45),
+	idComponente INT PRIMARY KEY,
+    tipoComponente VARCHAR(45),
+    nome VARCHAR(45),
+    modelo VARCHAR(45),
+    fabricante VARCHAR(45),
+    dispositivoConectadoUSB VARCHAR(45)
+);
+
+CREATE TABLE ConfiguracaoComponente(
 fkMaquina INT,
 FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
 fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
-);
-
-CREATE TABLE MetricaDisco(
-idMetricaDisco INT PRIMARY KEY,
+FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
 fkComponente INT,
 FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Componente(idComponente),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
-tamanho DECIMAL,
-leitura DECIMAL,
-escrita DECIMAL,
-tempoTransferencia DECIMAL,
-metricaDisco VARCHAR(45)
+quantidade VARCHAR(45),
+tamanho VARCHAR(45)
 );
 
-CREATE TABLE MetricaCPU(
-idMetricaCPU INT PRIMARY KEY,
-fkComponente INT,
-FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
+CREATE TABLE Metrica(
+idMetrica INT PRIMARY KEY,
 fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Componente(idComponente),
+FOREIGN KEY (fkMaquina) REFERENCES ConfiguracaoComponente (fkComponente),
 fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
+FOREIGN KEY (fkEmpresa) REFERENCES ConfiguracaoComponente (fkEmpresa),
+fkComponente INT,
+FOREIGN KEY (fkComponente) REFERENCES ConfiguracaoComponente(fkComponente),
+horario DATETIME,
+memoriaEmUso DECIMAL,
+memoriaDisponivel DECIMAL,
+memoriaTotal DECIMAL,
 usoCPU DECIMAL,
 usoMemoria DECIMAL,
-bytesUsados DECIMAL,
-memoriaVirtualUtilizada DECIMAL,
-metricaCPU VARCHAR(45)
-);
-
-CREATE TABLE MetricaMemoria(
-idMetricaMemoria INT PRIMARY KEY,
-fkComponente INT,
-FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Componente(idComponente),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
-emUso DECIMAL,
-disponivel DECIMAL,
-total DECIMAL
-);
-
-CREATE TABLE MetricaUSB(
-ifMetricaUSB INT PRIMARY KEY,
-dispositivoConectado VARCHAR(45),
-totalDispositivosConectados VARCHAR(45),
-fkPeriferico INT,
-fkComponente INT,
-FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+memoriaVirtualUsada DECIMAL,
+tamahnoDisco DECIMAL,
+leituraDisco DECIMAL,
+escritaDisco DECIMAL,
+tempoTransferencia DECIMAL,
+totalDispositivoConectado INT
 );
 
 INSERT INTO Empresa VALUES
@@ -142,35 +119,32 @@ INSERT INTO RegistroAtividade VALUES
 SELECT * FROM RegistroAtividade;
     
 INSERT INTO MetricaMouse VALUES
-	(1, 23, 75, null, 1, 111111);
+	(1, 23, 75, 1, 111111, 5000, "2023-04-12 20:21:30");
     
 SELECT * FROM MetricaMouse;
     
 INSERT INTO Componente VALUES
-	(1, 'Componente', 'componente', 'AZUS', 1, 111111);
+	(1, "RAM", "ASUS", "MK2", "ASUS", null);
     
 SELECT * FROM Componente;
     
-INSERT INTO MetricaDisco VALUES
-	(1, 1, 1, 111111, 8.0, 3.2, 2.2, 5.4, 'bytes');
+INSERT INTO ConfiguracaoComponente VALUES
+	(1, 111111, 1, "2", "8GB");
     
-SELECT * FROM MetricaDisco;
+SELECT * FROM ConfiguracaoComponente;
+
+INSERT INTO Metrica (idMetrica, fkMaquina, fkEmpresa, fkComponente) VALUES
+	(1, 1, 111111, 1);
     
-INSERT INTO MetricaCPU VALUES
-	(1, 1, 1, 111111, 8.123, 2.456, 3.789, 4.123, 'bytes');
-    
-SELECT * FROM MetricaCPU;
-    
-INSERT INTO MetricaMemoria VALUES
-	(1, 1, 1, 111111, 1111.2, 222222.4, 333333.2);
-    
-SELECT * FROM MetricaMemoria;
-    
-INSERT INTO MetricaUSB VALUES
-	(1, 'Teclado', '2', null, 1, 1, 111111);
-    
-SELECT * FROM MetricaUSB;
+SELECT * FROM Metrica;
     
 SELECT * FROM Empresa 
 	JOIN Usuario AS usuario ON usuario.fkEmpresa = idEmpresa
     JOIN Maquina AS maquina ON maquina.fkEmpresa = idEmpresa;
+    
+SELECT * FROM ConfiguracaoComponente AS cc
+	JOIN Componente ON cc.fkComponente = idComponente
+    JOIN Metrica ON cc.fkMaquina = metrica.fkMaquina;
+    
+SELECT * FROM MetricaMouse
+	JOIN Maquina ON fkMaquina = idMaquina;
