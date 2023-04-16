853293e771fb68ef2a531c713b8e18cfb5e88d0f
BRyuTakahashi <bruno.takahashi@sptech.school>
2023-04-16 10:12:01 -0300

    alterando tabelas conforme novo Script 15/04

--- site/src/database/nexusCenter-Script.sql
+++ site/src/database/nexusCenter-Script.sql
@@ -4,98 +4,106 @@ CREATE DATABASE NexusCenter;
 USE NexusCenter;
 
 CREATE TABLE Empresa(
-idEmpresa CHAR(6) PRIMARY KEY,
-razaoSocial VARCHAR(120),
-CNPJ CHAR(14),
-email VARCHAR(45),
-tel CHAR(14)
+	idEmpresa CHAR(6) PRIMARY KEY,
+	razaoSocial VARCHAR(120),
+	CNPJ CHAR(14),
+	email VARCHAR(45),
+	tel CHAR(14)
 );
 
 CREATE TABLE Usuario(
-idUser INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(45),
-email VARCHAR(45),
-senha VARCHAR(45),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+	idUser INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	email VARCHAR(45),
+	senha VARCHAR(45),
+	fkEmpresa CHAR(6),
+	FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE Maquina(
-idMaquina INT PRIMARY KEY AUTO_INCREMENT,
-nomeDoUsuario VARCHAR(45),
-patrimonio VARCHAR(45),
-senha VARCHAR(45),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+	idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+	nomeDoUsuario VARCHAR(45),
+	patrimonio VARCHAR(45),
+	senha VARCHAR(45),
+	fkEmpresa CHAR(6),
+	FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE MetricaMouse(
-idMetrica INT PRIMARY KEY AUTO_INCREMENT,
-cordenadaX INT,
-cordenadaY INT,
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
-qntClicks VARCHAR(45),
-dataHora DATETIME
+	idMetrica INT PRIMARY KEY AUTO_INCREMENT,
+	cordenadaX INT,
+	cordenadaY INT,
+	qntClicks VARCHAR(45),
+	dataHora DATETIME,
+	fkMaquina INT,
+	FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
+	fkEmpresa CHAR(6),
+	FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE RegistroAtividade(
-fkUsuario INT,
-FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUser),
-fkUsuarioEmpresa CHAR(6),
-FOREIGN KEY (fkUsuarioEmpresa) REFERENCES Empresa(idEmpresa),
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
-sistemaOperacional VARCHAR(45),
-fabricante VARCHAR(45),
-arquitetura VARCHAR(45),
-inicializado DATETIME,
-tempoDeAtividade VARCHAR(45),
-permissoes VARCHAR(45)
+	fkUsuario INT,
+	FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUser),
+	fkEmpresa CHAR(6),
+	FOREIGN KEY (fkEmpresa) REFERENCES Maquina(fkEmpresa),
+	fkMaquina INT,
+	FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
+	sistemaOperacional VARCHAR(45),
+	fabricante VARCHAR(45),
+	arquitetura VARCHAR(45),
+	inicializado DATETIME,
+	tempoDeAtividade VARCHAR(45)
 );
 
 CREATE TABLE Componente(
 	idComponente INT PRIMARY KEY,
-    tipoComponente VARCHAR(45),
-    nome VARCHAR(45),
     modelo VARCHAR(45),
-    fabricante VARCHAR(45),
-    dispositivoConectadoUSB VARCHAR(45)
+    tipoComponente VARCHAR(45)
 );
 
 CREATE TABLE ConfiguracaoComponente(
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
-fkComponente INT,
-FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
-quantidade VARCHAR(45),
-tamanho VARCHAR(45)
+	fkMaquina INT,
+    FOREIGN KEY (fKMaquina) REFERENCES Maquina(idMaquina),
+    fkEmpresa CHAR(6),
+    FOREIGN KEY (fkEmpresa) REFERENCES Maquina(fkEmpresa),
+    fkComponente INT,
+    FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente),
+    capacidade DECIMAL,
+    quantidade INT
 );
 
 CREATE TABLE Metrica(
-idMetrica INT PRIMARY KEY,
-fkMaquina INT,
-FOREIGN KEY (fkMaquina) REFERENCES ConfiguracaoComponente (fkComponente),
-fkEmpresa CHAR(6),
-FOREIGN KEY (fkEmpresa) REFERENCES ConfiguracaoComponente (fkEmpresa),
-fkComponente INT,
-FOREIGN KEY (fkComponente) REFERENCES ConfiguracaoComponente(fkComponente),
-horario DATETIME,
-memoriaEmUso DECIMAL,
-memoriaDisponivel DECIMAL,
-memoriaTotal DECIMAL,
-usoCPU DECIMAL,
-usoMemoria DECIMAL,
-memoriaVirtualUsada DECIMAL,
-tamahnoDisco DECIMAL,
-leituraDisco DECIMAL,
-escritaDisco DECIMAL,
-tempoTransferencia DECIMAL,
-totalDispositivoConectado INT
+	idMetrica INT PRIMARY KEY AUTO_INCREMENT,
+    memoriaEmUso DECIMAL,
+    memoriaDisponivel DECIMAL,
+    memoriaTotal DECIMAL,
+    usooCPU DECIMAL,
+    usoMemoria DECIMAL,
+    bytesUsados DECIMAL,
+    memoriaVirtualUsada DECIMAL,
+    tamanhoDisco DECIMAL,
+    leituraDisco DECIMAL,
+    escritaDisco DECIMAL,
+    fkMaquina INT,
+    FOREIGN KEY (fkMaquina) REFERENCES ConfiguracaoComponente(fkMaquina),
+    fkEmpresa CHAR(6),
+    FOREIGN KEY (fkEmpresa) REFERENCES ConfiguracaoComponente(fkEmpresa),
+    fkComponente INT,
+    FOREIGN KEY (fkComponente) REFERENCES ConfiguracaoComponente(fkComponente)
+);
+
+CREATE TABLE Alerta(
+	idAlerta INT,
+    destinario VARCHAR(45),
+    dataHora DATETIME,
+    fkMetrica INT,
+    FOREIGN KEY(fkMetrica) REFERENCES Metrica(idMetrica),
+    fkMaquina INT,
+    FOREIGN KEY(fkMaquina) REFERENCES Metrica(fkMaquina),
+    fkEmpresa CHAR(6),
+    FOREIGN KEY (fkEmpresa) REFERENCES Metrica(fkEmpresa),
+    fkComponente INT,
+    FOREIGN KEY (fkComponente) REFERENCES Metrica(fkComponente)
 );
 
 INSERT INTO Empresa VALUES
@@ -114,29 +122,34 @@ INSERT INTO Maquina VALUES
 SELECT * FROM Maquina;
     
 INSERT INTO RegistroAtividade VALUES
-	(1, 111111, 1, 'Windows', 'Samsung', 'X', '2023-04-11 11:19:30','4h', 'nenhuma');
+	(1, 111111, 1, 'Windows', 'Samsung', null, '2023-04-16 9:53:12', '4h');
     
 SELECT * FROM RegistroAtividade;
     
 INSERT INTO MetricaMouse VALUES
-	(1, 23, 75, 1, 111111, 5000, "2023-04-12 20:21:30");
+	(1, 53, 206, 30, '2023-04-16 9:58:42', 1, 111111);
     
 SELECT * FROM MetricaMouse;
-    
+
 INSERT INTO Componente VALUES
-	(1, "RAM", "ASUS", "MK2", "ASUS", null);
+	(1, 'Memoria RAM', 'ASUS');
     
 SELECT * FROM Componente;
-    
+
 INSERT INTO ConfiguracaoComponente VALUES
-	(1, 111111, 1, "2", "8GB");
+	(1, 111111, 1, FORMAT(5, 1), 1);
     
-SELECT * FROM ConfiguracaoComponente;
+SELECT * FROM ConfiguracaoCOmponente;
 
 INSERT INTO Metrica (idMetrica, fkMaquina, fkEmpresa, fkComponente) VALUES
 	(1, 1, 111111, 1);
     
 SELECT * FROM Metrica;
+
+INSERT INTO Alerta VALUES
+	(1, 'Ryu', '2023-04-16 10:07:23', 1, 1, 111111, 1);
+    
+SELECT * FROM Alerta;
     
 SELECT * FROM Empresa 
 	JOIN Usuario AS usuario ON usuario.fkEmpresa = idEmpresa
