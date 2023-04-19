81babe0686d9161411d680e553158d8ac2eea852
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-04-19 11:07:41 -0300

    Script local das tabelas atualizado com fkEmpresa

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -22,3 +22,15 @@ FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 SELECT * FROM usuario;
+
+
+CREATE TABLE maquina (
+idMaquina int primary key auto_increment,
+nomeUsuario varchar(45),
+patrimonio varchar(20),
+senha varchar(30),
+fkEmpresa CHAR(6),
+FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+);
+
+SELECT * FROM maquina;
