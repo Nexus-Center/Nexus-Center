17e47b974c351511577e5e2fb33c6000240ff449
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-04-21 19:54:36 -0300

    Mudando para Produção

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "nexus-center.database.windows.net",
+    database: "nexus-center",
+    user: "admin-nexus-center",
+    password: "#Gfgrupo6",
     pool: {
         max: 10,
         min: 0,
