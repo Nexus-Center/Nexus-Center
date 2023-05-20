29186f2bbac0be1ae2df4f3554b6afec53f23f5f
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-05-20 17:00:26 -0300

    Correção de bug nos gráficos

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -11,12 +11,12 @@ function carregarDadosPeriodo(fkEmpresa) {
         on q.fkParametroAlerta = m.idMetrica
         WHERE
         q.dataHora >= DATEADD(day, - 1, GETDATE())
-             AND m.fkEmpresa = ${fkEmpresa} group by q.statusAlerta;`
+             AND m.fkEmpresa = ${fkEmpresa} group by q.statusAlerta ORDER BY FIELD(statusAlerta, 'Ideal', 'Atencao', 'Alerta');`
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = 
-        `select count(*) as quantidade from AlertaDashboard as q join metrica as m on q.fkParametroAlerta = m.idMetrica
-        where q.dataHora > now() - interval 24 hour and m.fkEmpresa = ${fkEmpresa} group by q.statusAlerta ;`
+        `select count(*) as quantidade, statusAlerta from AlertaDashboard as q join metrica as m on q.fkParametroAlerta = m.idMetrica
+        where q.dataHora > now() - interval 24 hour and m.fkEmpresa = ${fkEmpresa} group by q.statusAlerta ORDER BY FIELD(statusAlerta, 'Ideal', 'Atencao', 'Alerta');`
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
